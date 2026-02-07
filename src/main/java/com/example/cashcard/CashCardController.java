cba40e058b3adb05914e2f239991594609423ebf
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-02-07 14:43:08 -0300

    Spring Academy H2 database in memory

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package com.example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
 
         if (requestedId.equals(99L)) {
             CashCard cashCard = new CashCard(99L, 123.45);
